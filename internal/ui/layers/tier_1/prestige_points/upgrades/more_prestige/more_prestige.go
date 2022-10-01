ad7e9fc8b5d52f212d810cb8c6dbfb6bc425122b
Victor Bersy <victor.bersy@hey.com>
2022-10-01 18:53:27 +0200

    Add "more prestige" upgrade to prestige points

---
+++ internal/ui/layers/tier_1/prestige_points/upgrades/more_prestige/more_prestige.go
@@ -0,0 +1,43 @@
+package more_prestige
+
+import (
+	"github.com/VictorBersy/prestige-cli/internal/ui/layers"
+	"github.com/VictorBersy/prestige-cli/internal/ui/points"
+	"github.com/VictorBersy/prestige-cli/internal/ui/upgrades"
+)
+
+type MorePrestige struct {
+	Points         *points.Points
+	PrestigePoints *layers.Model
+	Upgrade        *upgrades.Model
+}
+
+func NewModel(pp *layers.Model, points *points.Points) MorePrestige {
+	mp := MorePrestige{
+		Points:         points,
+		PrestigePoints: pp,
+		Upgrade: &upgrades.Model{
+			Name:        "More Prestige",
+			Description: "Prestige Point gain is increased by 80%.",
+			Unlocked:    true,
+			Cost:        20,
+		},
+	}
+	return mp
+}
+
+func (mp *MorePrestige) Buy() {
+	mp.PrestigePoints.Amount = mp.Upgrade.Buy(mp.PrestigePoints.Amount)
+}
+
+func (mp *MorePrestige) Tick() {
+}
+
+func (mp *MorePrestige) GetModel() *upgrades.Model {
+	return mp.Upgrade
+}
+
+func Fetch(layer *layers.Model, points *points.Points) (upgrade upgrades.Upgrade) {
+	upgradeModel := NewModel(layer, points)
+	return &upgradeModel
+}
