d6d15143909bed15c41eb41c25e61cce707e5509
Alyx <github@alyx.pink>
2022-11-03 13:24:12 +0100

    Remove unused config and defaults stuff

--- internal/ui/context/context.go
+++ internal/ui/context/context.go
@@ -1,18 +1,8 @@
 package context
 
-import (
-	"github.com/VictorBersy/prestige-cli/internal/config"
-)
-
 type ProgramContext struct {
 	ScreenHeight int
 	ScreenWidth  int
 	Width        int
 	Height       int
-	Config       *config.Config
-	Layer        config.LayerName
-}
-
-func (ctx *ProgramContext) GetLayersConfig() []config.LayerConfig {
-	return ctx.Config.Layers
 }
