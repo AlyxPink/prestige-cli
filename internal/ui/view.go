55bc82d6a16fb0e17a78b87c3a6aa8d16bb981c6
Victor Bersy <victor.bersy@hey.com>
2022-11-03 23:00:28 +0100

    Rename method to clearer intention

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -105,7 +105,7 @@ func (m Model) tierTitle(layer layers.Layer) string {
 func (m Model) gameGoal() string {
 	s := strings.Builder{}
 	s.WriteString(fmt.Sprintln(lipgloss.NewStyle().Bold(true).Render("Reach e3.140e16 points to beat the game!")))
-	s.WriteString(fmt.Sprintln(lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("You have %.2f points! (%.2f/sec)", m.Points.Count(), m.TickAmount()))))
+	s.WriteString(fmt.Sprintln(lipgloss.NewStyle().Bold(true).Render(fmt.Sprintf("You have %.2f points! (%.2f/sec)", m.Points.Count(), m.TickPerSecond()))))
 	return lipgloss.NewStyle().
 		Align(lipgloss.Center).
 		Width((m.ctx.ScreenWidth / 12) * 10).
