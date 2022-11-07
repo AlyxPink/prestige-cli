59e9eb4b040135fa7dbb4e6b60b71f7eb5e795a9
Victor Bersy <victor.bersy@hey.com>
2022-11-07 23:42:54 +0100

    Add new achievements to prestige points

---
+++ internal/ui/layer/prestige_points/achievement/prestige_all_the_way.go
@@ -0,0 +1,40 @@
+package achievement
+
+import (
+	"github.com/VictorBersy/prestige-cli/internal/ui/layer"
+)
+
+type prestigeAllTheWay struct {
+	Achievement *layer.ModelAchievement
+}
+
+func FetchPrestigeAllTheWay(layers *layer.Layers) (Achievement layer.Achievement) {
+	model := prestigeAllTheWay{
+		Achievement: &layer.ModelAchievement{
+			Name:        "Prestige all the Way",
+			Description: "Purchase 3 Prestige Upgrades. Reward: Gain 10% more Prestige Points.",
+			Layers:      layers,
+		},
+	}
+	return &model
+}
+
+func (m *prestigeAllTheWay) Tick() {
+	if !m.Achievement.Achieved && m.Done() {
+		m.Achievement.Achieved = true
+	}
+}
+
+func (m *prestigeAllTheWay) Done() bool {
+	return len(m.Model().Layers.PrestigePoints.Model().ListUpgradeEnabled()) >= 3
+}
+
+func (m *prestigeAllTheWay) Effect() {}
+
+func (m *prestigeAllTheWay) Unlocked() bool {
+	return true
+}
+
+func (m *prestigeAllTheWay) Model() *layer.ModelAchievement {
+	return m.Achievement
+}
